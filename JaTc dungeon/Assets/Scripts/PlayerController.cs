4dff677ce46d7388fe5878751a51af54556f3529
Jair Torres <jairalex0306.jatc@gmail.com>
2022-10-27 20:10:10 -0500

    update

--- JaTc dungeon/Assets/Scripts/PlayerController.cs
+++ JaTc dungeon/Assets/Scripts/PlayerController.cs
@@ -4,13 +4,22 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField] private float moveSpeed = 10f;
-    [SerializeField] private float jumpForce = 15f;
-    [SerializeField] private float gravityScale = 10f;
-
-    private Vector3 moveDirection;
-
+    [Header("Moviment Player")]
     [SerializeField] private CharacterController characterController;
+    [SerializeField] private float moveSpeed = 2f;
+    private float moveHorizontal;
+    private float moveVertical;
+    private Vector3 playerInput;
+    private Vector3 playerMove;
+    [SerializeField] private float playerGravity = 20f;
+    private float fallVelocity;
+    [Range(1, 4f)][SerializeField] private float jumpForce = 3f;
+    [SerializeField] private Animator playerAnimation;
+
+    [Header("Camera")]
+    [SerializeField] private Camera mainCamera;
+    private Vector3 cameraForward;
+    private Vector3 cameraRight;
 
     // Start is called before the first frame update
     void Start()
@@ -21,21 +30,70 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float yStore = moveDirection.y;
+        moveHorizontal = Input.GetAxis("Horizontal");
+        moveVertical = Input.GetAxis("Vertical");
 
-        // Moviment
-        moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0f, Input.GetAxisRaw("Vertical"));
-        moveDirection = moveDirection * moveSpeed;
-        moveDirection.y = yStore;
+        playerInput = new Vector3(moveHorizontal, 0f, moveVertical);
+        playerInput = Vector3.ClampMagnitude(playerInput, 1);
 
-        // Jump
-        if (Input.GetButtonDown("Jump") && moveDirection.y < 1.1f)
+        playerAnimation.SetFloat("playerVelocity", playerInput.magnitude * moveSpeed);
+
+        CameraDirection();
+
+        playerMove = playerInput.x * cameraRight + playerInput.z * cameraForward;
+        playerMove *= moveSpeed;
+
+        characterController.transform.LookAt(characterController.transform.position + playerMove);
+
+        SetGravityPlayer();
+
+        PlayerSkills();
+
+        //Moviment
+        characterController.Move(moveSpeed * Time.deltaTime * playerMove);
+
+        Debug.Log(characterController.velocity.magnitude);
+    }
+
+    //Function to know where the player is looking
+    public void CameraDirection()
+    {
+        cameraForward = mainCamera.transform.forward;
+        cameraRight = mainCamera.transform.right;
+
+        cameraForward.y = 0;
+        cameraRight.y = 0;
+
+        cameraForward = cameraForward.normalized;
+        cameraRight = cameraRight.normalized;
+    }
+
+    //Function to get the falling speed
+    public void SetGravityPlayer()
+    {
+        if (characterController.isGrounded)
         {
-            moveDirection.y = jumpForce;
+            fallVelocity = -playerGravity * Time.deltaTime;
+            playerMove.y = fallVelocity;
         }
+        else
+        {
+            fallVelocity -= playerGravity * Time.deltaTime;
+            playerMove.y = fallVelocity;
+            playerAnimation.SetFloat("playerVelocityVertical", characterController.velocity.y);
+        }
+        playerAnimation.SetBool("isGrounded", characterController.isGrounded);
+    }
 
-        moveDirection.y += Physics.gravity.y * Time.deltaTime * gravityScale;
-
-        characterController.Move(moveDirection * Time.deltaTime);
+    //Function for player skills
+    public void PlayerSkills()
+    {
+        //Jump
+        if (characterController.isGrounded && Input.GetButtonDown("Jump"))
+        {
+            fallVelocity = jumpForce;
+            playerMove.y = fallVelocity;
+            playerAnimation.SetTrigger("playerJump");
+        }
     }
 }
